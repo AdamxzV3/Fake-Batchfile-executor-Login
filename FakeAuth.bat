a850ddb6a45288b14f883e3be2cd335e47f9db6a
AdamxzV3 <99908796+AdamxzV3@users.noreply.github.com>
2023-01-21 21:21:12 +0100

    Add files via upload

---
+++ FakeAuth.bat
@@ -0,0 +1,82 @@
+Title Cracked ScriptWare V2
+
+rem Replace Texts!, you can do anything you want with this file its just a basic fake login!
+rem i will place some shitty checkers to make it more realistic, python versions are coming soon too
+
+
+
+
+
+@echo off
+:start
+cls
+echo -Installation (Will Download Important Files.) 
+echo -Create (Create your Cracked Acc) 
+echo -Login(Logins to your cracked acc) 
+echo -Open (Opens ScriptWare, SynX etc..) 
+echo ===============================================
+set /p PROGRAM= What do you want to do?:
+cls
+goto %PROGRAM%
+
+
+
+
+
+
+
+
+:Installation
+title Script Ware Cracked  rem replace with any shit? you can make anything and rename
+echo Downloading important files
+timeout /T 3 /NOBREAK > nul
+cls
+echo Downloading.
+timeout /T 1 /NOBREAK > nul
+cls
+Echo Downloading..
+timeout /T 1 /NOBREAK > nul
+cls
+cls
+Echo Downloading...
+timeout /T 1 /NOBREAK > nul
+cls
+cls
+Echo Downloading....
+cls
+Echo Downloading.....
+timeout /T 1 /NOBREAK > nul
+cls
+echo Sucessfully Downloaded
+pause
+goto start
+
+
+:login
+set /p User= Username?
+echo Hello %User%
+echo Succesfully Found...
+timeout /T 2 /NOBREAK > nul
+cls
+set /p Password= Password?
+echo Successfully Logged In.
+pause
+goto start
+
+:create
+cls
+set /p Username1= What do you want to be your Username?:
+echo %Username1% > user.bin
+timeout /T 2 /NOBREAK > nul
+cls
+set /p PASSWORD1= What do you want to be your password?:
+echo %PASSWORD1% > data.bin
+pause
+goto start
+
+:Open
+cls
+Echo Your Program is starting....
+start RobloxPlayerBeta.exe
+pause
+goto start
